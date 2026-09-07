674071c0aeda5aa54878dc23866cd6dca8a5bf32
4aman24-cmyk <4aman24@gmail.com>
2026-09-07 23:16:37 +0300

    Add repositories to pluginManagement in settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -7,6 +7,12 @@ pluginManagement {
         flutterSdkPath
     }
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
 
 plugins {
