9951eaf3f79474f614ec31829c94c02fc96e4376
4aman24-cmyk <4aman24@gmail.com>
2026-09-09 22:08:55 +0300

    Fix MainActivity.kt syntax error

--- android/app/src/main/kotlin/com/example/stadiome_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/stadiome_app/MainActivity.kt
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 package com.example.stadiome_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
-=======
-package com.example.stadiome_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
->>>>>>> 5b5f6f692c892dcc1e3e21dd5a3f37fd7ddbbeb1
+class MainActivity: FlutterActivity() {
+}
