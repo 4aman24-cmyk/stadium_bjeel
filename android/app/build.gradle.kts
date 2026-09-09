5303a419c97b51a9f628a3d9d88b7e1f0510cddb
4aman24-cmyk <4aman24@gmail.com>
2026-09-09 20:21:19 +0300

    Fix codemagic yaml paths

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -43,49 +42,3 @@ android {
 flutter {
     source = "../.."
 }
-=======
-plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
-}
-
-android {
-    namespace = "com.example.stadiome_app"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-
-    defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.stadiome_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
-
-    buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
-        }
-    }
-}
-
-flutter {
-    source = "../.."
-}
->>>>>>> 5b5f6f692c892dcc1e3e21dd5a3f37fd7ddbbeb1
